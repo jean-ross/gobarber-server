670d4b157d937d63efa0a9e60e82d18794b6d880
Jean Ross <jeancmartins@icloud.com>
2020-10-01 06:44:27 -0300

    Container refactor

---
+++ src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,15 @@
+import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+
+import IMailTemplateProvider from './models/IMailTemplateProvider';
+
+import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
+
+const providers = {
+  handlebars: HandlebarsMailTemplateProvider,
+};
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  providers.handlebars,
+);
